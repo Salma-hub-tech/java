226ac7bd3617c386d59b04b91e8ca19bc4c75f72
Salma Abdelaziz <abdelazizsalma790@gmail.com>
2024-12-06 00:35:53 +0200

    Create even and odd.java

---
+++ even and odd.java
@@ -0,0 +1,23 @@
+mport java.util.Scanner;
+
+//Salma Abdelaziz
+class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a number:");
+        int n= scanner.nextInt();
+        if(n%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
+
+
+
+
+
+    }
+
+
+}
