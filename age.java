7a79bf1fd000141cffe79e2aab3b0643b8f60d20
Salma Abdelaziz <abdelazizsalma790@gmail.com>
2024-12-06 00:52:13 +0200

    Create age.java

---
+++ age.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//Salma Abdelaziz
+class Main {
+
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age");
+        int age= scanner.nextInt();
+        if(age>30){
+            System.out.println("you are old");
+        }else{
+            System.out.println("you are young");
+        }
+
+
+
+
+
+
+    }
+
+
+}
