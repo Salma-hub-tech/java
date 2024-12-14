9e28c4eff6950056a452c511ee10adbf1d88d0f9
Salma Abdelaziz <abdelazizsalma790@gmail.com>
2024-12-14 20:40:01 +0200

    Update

---
+++ multiplication table of 2.java
@@ -0,0 +1,11 @@
+//Salma Abdelaziz
+public class Main {
+    public static void main(String[] args) {
+        int number=2;
+        for(int i=1; i<=12; i++){
+
+            System.out.println(number + "*" + i + "=" + i*2);
+        }
+
+    }
+}
